955ea138f8ee0e4d403fc11e3dbd77b4b09cd332
andrewprog97 <andrewprog97@gmail.com>
2023-10-09 23:49:51 +0300

    change port

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 
+const PORT = 3000;
+
 const app = express();
 
 app.use(express.static("./public"));
 
-app.listen(80, () => console.log("Server has been launched at port 80"));
+app.listen(PORT, () => console.log("Server has been launched at port " + PORT));
